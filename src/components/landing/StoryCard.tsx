09e0ab1d87d4cd3e7d7022b40f2bebaa76aaf96e
Rogersullivancodes <chalcedony2020@proton.me>
2025-07-04 03:10:37 +0100

    Update StoryCard.tsx

--- src/components/landing/StoryCard.tsx
+++ src/components/landing/StoryCard.tsx
@@ -1,1 +1,22 @@
+// src/components/landing/StoryCard.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+interface Story {
+  imageUrl: string;
+  title: string;
+  slug: string;
+}
+
+export const StoryCard: React.FC<{ story: Story }> = ({ story }) => {
+  return (
+    <article>
+      <Link to={`/articles/${story.slug}`} className="group">
+        <img src={story.imageUrl} alt={story.title} className="w-full h-auto object-cover mb-2" />
+        <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200 group-hover:underline">
+          {story.title}
+        </h2>
+      </Link>
+    </article>
+  );
+};
