73ae968e15b9fd43e29a680f066701a166bd4728
Rogersullivancodes <chalcedony2020@proton.me>
2025-07-08 09:48:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { AdminProvider } from "@/contexts/AdminContext";
-import Index from "./pages/Index";
+import { Index } from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import SignupComplete from "./pages/SignupComplete";
