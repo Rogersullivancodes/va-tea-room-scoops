69ba359a9a5f966afb8d0ae9b0c4932dab4680d3
Rogersullivancodes <chalcedony2020@proton.me>
2025-07-08 09:59:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import SplashPage from "./pages/SplashPage";// src/App.tsx
+import { SplashPage } from "./pages/SplashPage";// src/App.tsx
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,20 +7,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { AdminProvider } from "@/contexts/AdminContext";
 import { Index } from "./pages/Index";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import SignupComplete from "./pages/SignupComplete";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Memberships from "./pages/Memberships";
-import AdminLogin from "./pages/AdminLogin";
-import AdminDashboard from "./pages/AdminDashboard";
-import Profile from "./pages/Profile";
-import Articles from "./pages/Articles";
-import NotFound from "./pages/NotFound";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfUse from "./pages/TermsOfUse";
-import ArticleDetail from "./pages/ArticleDetail";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { SignupComplete } from "./pages/SignupComplete";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Memberships } from "./pages/Memberships";
+import { AdminLogin } from "./pages/AdminLogin";
+import {AdminDashboard} from "./pages/AdminDashboard";
+import { Profile } from "./pages/Profile";
+import { Articles } from "./pages/Articles";
+import { NotFound } from "./pages/NotFound";
+import { PrivacyPolicy } from "./pages/PrivacyPolicy";
+import { TermsOfUse } from "./pages/TermsOfUse";
+import { ArticleDetail } from "./pages/ArticleDetail";
 
 const queryClient = new QueryClient();
 
